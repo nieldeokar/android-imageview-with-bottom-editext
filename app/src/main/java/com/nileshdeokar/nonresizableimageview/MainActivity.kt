0ed9f468bf68b9231066023b38af746a6580f72a
Nilesh deokar <wilson@porsh.net>
2018-02-22 16:48:11 +0200

    Added glide for loading image

--- app/src/main/java/com/nileshdeokar/nonresizableimageview/MainActivity.kt
+++ app/src/main/java/com/nileshdeokar/nonresizableimageview/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.nileshdeokar.nonresizableimageview
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() , KeyboardHeightProvider.KeyboardHeightObserver  {
@@ -32,10 +37,32 @@ class MainActivity : AppCompatActivity() , KeyboardHeightProvider.KeyboardHeight
         }
 
         keyboardHeightProvider = KeyboardHeightProvider(this)
-        bottomEditor.post { initialY = bottomEditor.y; initialheightOfEditText = edit_message.height }
+        bottomEditor.post { initialY = bottomEditor.y; initialheightOfEditText = edit_url.height }
         parentActivityView.post { keyboardHeightProvider?.start() }
+
+        btn_send.setOnClickListener({ loadImage() })
+
     }
 
+    private fun loadImage() {
+        val url = edit_url.text.toString().trim()
+        if(url.isNotEmpty()) {
+            Glide.with(this)
+                    .load(url)
+                    .placeholder(R.drawable.loading)
+                    .error(R.drawable.error)
+                    .into(imgPreview)
+        }else{
+            Toast.makeText(this,getString(R.string.error_empty_url),Toast.LENGTH_LONG).show()
+        }
+        edit_url.setText("")
+        hideKeyboard()
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(parentActivityView.windowToken, 0)
+    }
 
     override fun onKeyboardHeightChanged(height: Int, orientation: Int) {
         var y = initialY
@@ -48,8 +75,8 @@ class MainActivity : AppCompatActivity() , KeyboardHeightProvider.KeyboardHeight
         }
         if (settledHeight == 0) { // keyboard is closed
 
-            if(edit_message.height != initialheightOfEditText){
-                y += (initialheightOfEditText - edit_message.height)
+            if(edit_url.height != initialheightOfEditText){
+                y += (initialheightOfEditText - edit_url.height)
             }
         } else {
             y = if (wasNegative && settledHeight != negativePixelHeight){
@@ -60,8 +87,8 @@ class MainActivity : AppCompatActivity() , KeyboardHeightProvider.KeyboardHeight
                 initialY - settledHeight
             }
 
-            if(edit_message.height != initialheightOfEditText){
-                y -= (edit_message.height - initialheightOfEditText)
+            if(edit_url.height != initialheightOfEditText){
+                y -= (edit_url.height - initialheightOfEditText)
             }
 
         }
